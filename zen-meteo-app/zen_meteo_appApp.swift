abcc2c3abc10fae3793a66943de6512a1a7eef56
angelodig <anreg@hotmail.it>
2021-01-11 08:42:20 +0100

    Initial Commit

---
+++ zen-meteo-app/zen_meteo_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  zen_meteo_appApp.swift
+//  zen-meteo-app
+//
+//  Created by Angelo Di Gianfilippo on 11/01/21.
+//
+
+import SwiftUI
+
+@main
+struct zen_meteo_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
